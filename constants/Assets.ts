0f840393309fe1703609d0cdb61baa163cefafb6
Aman Tiwari <amantiwari8861@gmail.com>
2026-04-15 16:29:49 +0530

    minor fixes

--- constants/Assets.ts
+++ constants/Assets.ts
@@ -1,5 +1,4 @@
 export const ASSETS = {
-  dsa: require('../assets/images/dsa.png'),
-  java: require('../assets/images/java.png'),
-  // Add more as they are generated
+  // dsa: require('../assets/images/dsa.png'),
+  // java: require('../assets/images/java.png'),
 };
